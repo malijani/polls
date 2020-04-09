a2c19de67519370205f1956c5c1d282b191fca78
malijani <alijanialijanvandmohammad@gmail.com>
2020-04-09 18:40:47 +0430

    Created empty view of index

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,3 +1,13 @@
+""" Views of polls app which renders output """
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Question, Choice
+
+
+def index(request):
+    """ Get questions and display them """
+    template = 'polls/index.html'
+    context = {
+
+    }
+    return render(request, template, context)
