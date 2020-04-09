901b954fbdbff7294c2d1b21ee0078ab35cc66fc
malijani <alijanialijanvandmohammad@gmail.com>
2020-04-09 18:17:22 +0430

    Registered models and Configured them

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -1,3 +1,30 @@
+""" Register polls models into admin area """
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+# Change Admin Site Header
+admin.site.site_header = "Pollster Admin"
+admin.site.site_title = "Pollster Admin Area"
+admin.site.index_title = "Welcome to the Pollster admin area"
+
+# Register models
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    """ ChoiceInline is a TabularInline that will show Choices in Question section """
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    """ Configurations of main QuestionAdmin model """
+    # fieldsets tuple : question_text , pub_date
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    # configure inlines
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
