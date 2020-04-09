5a32bb95cbcb5e8772ac953c47e65315cfb37de4
malijani <alijanialijanvandmohammad@gmail.com>
2020-04-09 21:11:28 +0430

    Polls app URLs are configured

--- pollster/polls/urls.py
+++ pollster/polls/urls.py
@@ -6,4 +6,7 @@ app_name = apps.PollsConfig.name
 
 urlpatterns = [
     path('', views.index, name='index'),
-]
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/result/', views.result, name='result'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    ]
